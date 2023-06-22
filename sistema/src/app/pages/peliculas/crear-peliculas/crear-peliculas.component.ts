ce82c58a32dd613eeabc838490064f5347661a05
dannycastilloo <dannycastillootiniano@gmail.com>
2023-06-22 12:06:17 -0500

    cambios

---
+++ sistema/src/app/pages/peliculas/crear-peliculas/crear-peliculas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-peliculas',
+  templateUrl: './crear-peliculas.component.html',
+  styleUrls: ['./crear-peliculas.component.css']
+})
+export class CrearPeliculasComponent {
+
+}
---
+++ sistema/src/app/pages/peliculas/editar-peliculas/editar-peliculas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-peliculas',
+  templateUrl: './editar-peliculas.component.html',
+  styleUrls: ['./editar-peliculas.component.css']
+})
+export class EditarPeliculasComponent {
+
+}
